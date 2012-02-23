8856ccb9641e39d56a21a19ead5e68c63cca7bc0
Sylvain Bouchard <sylvain@l3i.ca>
2012-02-23 12:04:12 -0500

    squelette de recherche.

---
+++ buildout/src/iron.core/src/iron/core/constantes.py
@@ -0,0 +1,44 @@
+#coding: utf-8
+
+ZONES = (
+('Beauport', ( 'Chutes-Montmorency', 'Vieux-Moulin') ),
+
+("Charlesbourg", ( 'Jésuites', 'Notre-Dame-des-Laurentides')),
+
+("La Cité-Limoilou", (
+    'Lairet',
+    'Maizerets',
+    'Montcalm',
+    'Saint-Jean-Baptiste',
+    'Saint-Roch',
+    'Saint-Sacrement',
+    'Saint-Sauveur',
+    'Vieux-Québec–Cap-Blanc–Colline-Parlementaire ',
+    'Vieux-Limoilou'
+)),
+
+("La Haute-Saint-Charles", (
+    'Châtels ',
+    'Lac-Saint-Charles',
+    'Loretteville',
+    'Saint-Émile',
+    'Val-Bélair',
+)),
+
+( "Les Rivières", (
+    'Duberger-Les Saules'
+    'Neufchâtel-Est–Lebourgneuf'
+    'Vanier'
+)),
+
+("Sainte-Foy–Sillery–Cap-Rouge", (
+    'Aéroport',
+    'Cap-Rouge ',
+    'Cité-Universitaire',
+    'Plateau',
+    'Pointe-de-Sainte-Foy',
+    'Saint-Louis',
+    'Sillery',
+))
+
+)
