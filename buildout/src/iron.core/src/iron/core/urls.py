9af1f25e85b2dbf467eaee42b703170a09cb7f5e
Sylvain Bouchard <sylvain@l3i.ca>
2012-02-23 17:08:55 -0500

    Url de view activité paramétrable

--- buildout/src/iron.core/src/iron/core/urls.py
+++ buildout/src/iron.core/src/iron/core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('iron.core.views',
     ('^search/$', 'search'),
     url('^list/$', 'results', {'mode':'liste'} ),
     url('^map/$', 'results', {'mode': 'map'}),
-    ('^activite/$', 'activite'),
+    ('^activite/(\d+)/$', 'activite'),
 
     (r"^evenements/$", 'iron.core.views.evenements'),
     (r"^evenements/search/$", 'iron.core.views.eventsearch'),
