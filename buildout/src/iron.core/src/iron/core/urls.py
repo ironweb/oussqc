10a7e8208c390abf29f0da2ed5ded3bed88698b6
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-02-23 07:18:45 -0500

    test json serialization

--- buildout/src/iron.core/src/iron/core/urls.py
+++ buildout/src/iron.core/src/iron/core/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url, include
 
 urlpatterns = patterns('',
     ('^$', 'iron.core.views.test'),
+    (r"^evenements/$", 'iron.core.views.evenements'),
     ('^screen/(\d+)$', 'iron.core.views.screen'),
 )
 
--- buildout/src/iron.core/src/iron/core/views.py
+++ buildout/src/iron.core/src/iron/core/views.py
@@ -1,7 +1,18 @@
 from django.http import HttpResponse
+from django.core import serializers
+from iron.core.models import Evenement
+
+json = serializer.get_serializer("json")()
 
 def test(request):
     return HttpResponse('yo')
 
 def screen(request, screen_no):
     return HttpResponse(screen_no)
+
+def evenements(request):
+    serializer = serializers.get_serializer("json")()
+    objects = Evenement.objects.all()
+    data = serializer.serialize(objects)
+    return HttpResponse(data)
+
