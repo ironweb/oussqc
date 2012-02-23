82bfa532907a27eee267118e4f610a914d180bad
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-02-23 06:00:43 -0500

    add small wrapper for extracting categories

---
+++ buildout/src/iron.core/src/iron/core/parser/categories.py
@@ -0,0 +1,60 @@
+import sys
+
+class CategoryParser(object):
+
+    def __init__(self):
+        pass
+
+    def categorie_evenements(self, evenements):
+
+        categories = set([
+            e['categorie'] for e in evenements
+        ])
+
+        return categories
+
+    def categorie_loisirs(self, loisirs):
+
+        categories = set([ x['nat'] for x in loisirs ])
+
+        return categories
+
+    def nested_categories(self, loisirs):
+        pass
+
+
+if __name__ == "__main__":
+
+    sys.path.append('.')
+    import iron.core.parser.evenements as evenements_parser
+    from iron.core.models import Categorie
+
+    if len(sys.argv) < 2:
+        print "Usage: %s TYPE XML_FILE (type: evenement or loisir)" % sys.argv[0]
+        sys.exit(1)
+
+    mode = sys.argv[1]
+    filepath = sys.argv[2]
+
+    f = open(filepath, 'r')
+
+    if mode == "evenement":
+        parser = evenements_parser.EvenementParser()
+        category_parser = CategoryParser()
+
+        evenements = parser.parse( f.read() )
+
+        categories = category_parser.categorie_evenements(evenements)
+
+        for category in categories:
+            model = Categorie()
+            model.UID = category
+            print model
+            model.save()
+
+    elif mode == "loisir":
+        parser = evenements_parser.EvenementParser()
+        category_parser = CategoryParser()
+
+        loisirs = parser
+
