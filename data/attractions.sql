785e0f7224cdcff81bb8eddb81d80b10f13f4ab3
Gregory Eric Sanderson <gzou2000@gmail.com>
2012-02-24 00:22:48 -0500

    some data for demo

---
+++ data/attractions.sql
@@ -0,0 +1,191 @@
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Clarendon Hotel',
+    'Hotel',
+    '57 Rue Sainte-Anne, Québec, QC G1R 3X4',
+    'Tous les employés voulaient savoir d’où nous venions, quels endroits nous avions visités, etc. Nous nous sentions en visite en famille plutôt qu’en voyage à l’étranger.',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Hôtel Le Germain-Dominion',
+    'Hotel',
+    '126 SAINT PIERRE RUE, Québec, Canada',
+    '',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Fairmont Le Château Frontenac',
+    'Hotel',
+    '1 Rue Des Carrieres, Québec, G1R 4P5',
+    'Perché sur le cap Diamant afin de mieux porter son regard sur le majestueux fleuve Saint-Laurent, Fairmont Le Château Frontenac n''est pas seulement un grand hôtel situé au coeur de Québec, il est le coeur même du Vieux-Québec. Un séjour de luxe au Château vous permettra d''explorer les merveilleux sites historiques situés à proximité de l''hôtel et de découvrir tout le cachet de la vieille ville fortifiée.',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Hotel du Vieux Quebec',
+    'Hotel',
+    '1190 Rue Saint-Jean, Québec, QC G1R 1S6',
+    'Venez découvrir notre établissement et profitez de son emplacement exceptionnel au coeur du Vieux-Québec. Hôtel du Vieux-Québec vous offre une gamme complète de services avec 45 chambres confortables et spacieuses à des prix abordables.  Notre hôtel charmant est le choix par excellence pour ceux et celles qui préfèrent un service personnalisé.',
+    NULL,
+    NULL
+);
+
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Auberge Place d''Armes',
+    'Hotel',
+    '24 rue Ste-Anne, Québec, QC G1R 3X3, Canada',
+    'Au coin des deux rues piétonnes de Québec, avec vue sur le Château Frontenac et la cathédrale Holy Trinity, près des musées, des boutiques, des restaurants… à deux pas de la Grande-Allée, du vieux port et du centre des Congrès.',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Restaurant Laurie Raphael Inc',
+    'Restaurant',
+    '117 Rue Dalhousie, Québec, QC G1K 9B7, Canada',
+    '',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Poisson D''Avril',
+    'Restaurant',
+    '115 Quai Saint-André, Québec, QC G1K 9E6, Canada',
+    '',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Aux Anciens Canadiens',
+    'Restaurant',
+    '34 Rue Saint-Louis, Quebec City, G1R 4P3',
+    '',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Restaurant Le Saint Amour',
+    'Restaurant',
+    '48 Sainte-Ursule, Québec, G1R 4E2, Canada',
+    '',
+    NULL,
+    NULL
+);
+
+INSERT INTO core_attraction
+(
+    NOM,
+    `TYPE`,
+    ADRESSE,
+    DESCRIPTION,
+    LATITUDE,
+    LONGITUDE
+)
+VALUES
+(
+    'Restaurant L''Echaudé',
+    'Restaurant',
+    '73 Rue du Sault au Matelot, Québec, QC G1K 8B9, Canada',
+    '',
+    NULL,
+    NULL
+);
